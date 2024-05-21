80d58de4752362fcbd5947431010241100dc8ee3
Satoshi <octocat@satoshidnc.com>
2024-05-21 19:11:00 -0400

    31m add bitcoin-connect

---
+++ pwa/lib/bitcoin-connect/bitcoin-connect.mjs
@@ -0,0 +1,6 @@
+/* esm.sh - @getalby/bitcoin-connect@3.4.2 */
+import "/lib/bitcoin-connect/sdk.mjs";
+import "/lib/bitcoin-connect/vanilla.js";
+import "/lib/bitcoin-connect/lightning-tools.mjs";
+import "/lib/bitcoin-connect/qrcode-generator.mjs";
+export * from "/lib/bitcoin-connect/bitcoin-connect.mjs";
